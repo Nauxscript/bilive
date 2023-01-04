c2d0303c400fcc6466a761ce0421d78c5d62ea76
nauxscript <nauxscript@gmail.com>
2023-01-04 10:21:31 +0800

    feat: color output

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+/* eslint-disable no-console */
 import Cac from 'cac'
+import chalk from 'chalk'
 
 // 从package.json中获取版本及名字
 import { name, version } from '../package.json'
@@ -6,8 +8,9 @@ import { name, version } from '../package.json'
 const cli = Cac(name)
 
 cli.command('start <roomid>', 'start to get the bullets from specific room').option('-s, --say', 'say every bullet').action((roomid: string, options) => {
-  // eslint-disable-next-line no-console
-  console.log(roomid, options)
+  console.log(chalk.yellow(roomid, options.say))
+  console.log(chalk.blue.bold('this is bule line'))
+  console.log(chalk.red.underline.redBright('this is red line'))
 })
 
 cli.help()
