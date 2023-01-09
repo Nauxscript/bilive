e25482ad1538af66777f4c8482e4f1c1ae675c4e
nauxscript <nauxscript@gmail.com>
2023-01-09 10:00:19 +0800

    feat: get room info

---
+++ src/fetchs.ts
@@ -0,0 +1,35 @@
+import { $fetch } from 'ohmyfetch'
+
+export interface RoomInfo {
+  uid: number
+  room_id: number
+  short_id: number
+  attention: number // 关注人数
+  online: number
+  is_portrait: boolean
+  description: string // 房间描述
+  live_status: number // 直播状态 0 未开播 / 1 已开播
+  area_id: number // 二级分区id
+  area_name: string // 二级分区名
+  parent_area_id: number // 一级分区id
+  parent_area_name: string // 一级分区名
+  background: string
+  title: string
+  user_cover: string
+  keyframe: string
+  live_time: string
+  tags: string // 标签，以 , 分割
+}
+
+interface RoomInfoResponse {
+  code: number
+  msg: string
+  message: string
+  data: RoomInfo
+}
+
+export const getRoomInfo = async (roomId: number) => {
+  const url = `https://api.live.bilibili.com/room/v1/Room/get_info?room_id=${roomId}`
+  const response = await $fetch<RoomInfoResponse>(url)
+  return response.code === 0 ? response.data : null
+}
